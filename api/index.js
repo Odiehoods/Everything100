806769aa85e4803f01cd68a52ea21ae45df00c05
Odiehoods <odiehoods@gmail.com>
2024-04-13 09:19:31 +0000

    database added

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+
+mongoose
+.connect('mongodb+srv://odiehoods:odiehoodsever@mern100.brzfe0z.mongodb.net/mernblog?retryWrites=true&w=majority&appName=mern100')
+.then( () => { console.log('Mongodb is connected');
+});
+
 
 const app = express();
 
